8305127ab8bcbdf033319949af331cf2e75326a6
hyesu <bobae04272@gmail.com>
2022-06-22 00:13:58 +0900

    - 응답 데이터타입 확인

--- DevProject/src/main/java/org/hdcd/controller/HomeController.java
+++ DevProject/src/main/java/org/hdcd/controller/HomeController.java
@@ -1,11 +1,6 @@
 package org.hdcd.controller;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -14,28 +9,34 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class HomeController {
 
+	@GetMapping("/registerForm")
+	public String registerForm() {
+		log.info("registerForm");
+		return "registerForm";
+	}
+
 //	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-	@GetMapping("/")
-	public String home(Locale locale, Model model) {
-		log.info("Welcome home! the client locale is" + locale + ".");
-
-		LocalDateTime now = LocalDateTime.now();
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 M월 d일 (E) a h시 m분 s초");
-		// Date date = new Date();
-		// DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
-		// DateFormat.LONG, locale);
-
-		String formattedDate = now.format(formatter);
-
-		model.addAttribute("serverTime", formattedDate);
-
-		return "home";
-	}
-
-	@GetMapping("/ajaxHome")
-	public String ajaxHome() {
-		return "ajaxHome";
-	}
+//	@GetMapping("/")
+//	public String home(Locale locale, Model model) {
+//		log.info("Welcome home! the client locale is" + locale + ".");
+//
+//		LocalDateTime now = LocalDateTime.now();
+//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 M월 d일 (E) a h시 m분 s초");
+//		// Date date = new Date();
+//		// DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
+//		// DateFormat.LONG, locale);
+//
+//		String formattedDate = now.format(formatter);
+//
+//		model.addAttribute("serverTime", formattedDate);
+//
+//		return "home";
+//	}
+//
+//	@GetMapping("/ajaxHome")
+//	public String ajaxHome() {
+//		return "ajaxHome";
+//	}
 
 }
--- DevProject/src/main/java/org/hdcd/domain/Member.java
+++ DevProject/src/main/java/org/hdcd/domain/Member.java
@@ -1,12 +1,19 @@
 package org.hdcd.domain;
 
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
-@ToString(exclude="userName")
+@Getter
+@Setter
+@ToString
 public class Member {
-	
-	private String userId;
-	private String password;
-	private String userName;
+
+	private String userId = "hongkd";
+	private String password = "1234";
+	private Address address;
+	private List<Card> cardList;
 
 }
